f429599d3790c0629765e15e79d17a2b56671069
Nathan Hazout <nathanh@il.ibm.com>
2015-07-08 10:53:50 +0300

    initial commit

---
+++ InvokingAdapterProceduresObjC/ViewController.m
@@ -0,0 +1,83 @@
+/*
+ *
+ COPYRIGHT LICENSE: This information contains sample code provided in source code form. You may copy, modify, and distribute
+ these sample programs in any form without payment to IBM® for the purposes of developing, using, marketing or distributing
+ application programs conforming to the application programming interface for the operating platform for which the sample code is written.
+ Notwithstanding anything to the contrary, IBM PROVIDES THE SAMPLE SOURCE CODE ON AN "AS IS" BASIS AND IBM DISCLAIMS ALL WARRANTIES,
+ EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, ANY IMPLIED WARRANTIES OR CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY,
+ FITNESS FOR A PARTICULAR PURPOSE, TITLE, AND ANY WARRANTY OR CONDITION OF NON-INFRINGEMENT. IBM SHALL NOT BE LIABLE FOR ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR OPERATION OF THE SAMPLE SOURCE CODE.
+ IBM HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS OR MODIFICATIONS TO THE SAMPLE SOURCE CODE.
+
+ */
+
+//
+//  ViewController.m
+//  nativeIOSApp
+
+#import "ViewController.h"
+#import "MyConnectListener.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+@synthesize textView;
+
+- (IBAction)doConnect:(UIButton *)sender {
+    [self updateView:@"Initializing..."];
+    NSLog(@"Initializing...");
+    MyConnectListener *connectListener = [[MyConnectListener alloc] initWithController:self];
+    [[WLClient sharedInstance] wlConnectWithDelegate:connectListener];
+
+}
+
+- (IBAction)doInvokeProcedure:(UIButton *)sender {
+    [self updateView:@"Invoking Procedure..."];
+    NSLog(@"Invoking Procedure...");
+
+    NSURL* url = [NSURL URLWithString:@"/adapters/RSSReader/getFeeds"];
+    WLResourceRequest* request = [WLResourceRequest requestWithURL:url method:WLHttpMethodGet];
+
+    [request setQueryParameterValue:@"['technology']" forName:@"params"];
+
+    [request sendWithCompletionHandler:^(WLResponse *response, NSError *error) {
+        NSString* resultText;
+        if(error != nil){
+            resultText = @"Invocation failure.";
+            resultText = [resultText stringByAppendingString: error.description];
+        }
+        else{
+            resultText = @"Invocation success.";
+            resultText = [resultText stringByAppendingString:response.responseText];
+        }
+        [self updateView:resultText];
+    }];
+
+}
+
+- (void) updateView:(NSString *)response {
+    [textView setText:response];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"viewDidLoad called.");
+}
+
+- (void)viewDidUnload
+{
+    [self setTextView:nil];
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
